dbcc91f931a81ea2318663fec56fb555d52a2cae
Tristan Cacqueray <tdecacqu@redhat.com>
2020-09-02 13:51:30 +0000

    Initial summon
    Change-Id: I3951a4eeac8cb962963516ca5d2f04e08bfc0ac2

---
+++ Shakefile.hs
@@ -0,0 +1,16 @@
+-- Interpret using this command:
+--    podman run -it --rm -v $(pwd):/data:Z quay.io/software-factory/shake-factory
+--
+-- Learn more at: https://softwarefactory-project.io/cgit/software-factory/shake-factory/tree/README.md
+
+import Development.Shake
+import ShakeFactory
+import ShakeFactory.Dhall
+
+main = shakeMain $ do
+  want $ [".zuul.yaml"]
+  ".zuul.yaml" %> dhallZuulAction "(./.sf.dhall).zuul"
+  phony "docs" $ do
+    cmd_ "rm -Rf build/docs"
+    cmd_ "tox -edocs"
+    cmd_ "mv build/html/pynotedb build/docs"
