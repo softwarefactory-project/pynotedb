41a5ab15abb0f49dbfc5b0c529b32c93e8dd7a6e
Tristan Cacqueray <tdecacqu@redhat.com>
2020-09-04 18:17:37 +0000

    Fix publish-docs job by installing pdoc3
    Change-Id: I95d7b8039868de0fb1cd6b91ebb5e1f07e4b5406

--- Shakefile.hs
+++ Shakefile.hs
@@ -3,14 +3,38 @@
 --
 -- Learn more at: https://softwarefactory-project.io/cgit/software-factory/shake-factory/tree/README.md
 
+import Control.Monad (unless)
 import Development.Shake
 import ShakeFactory
 import ShakeFactory.Dhall
 
-main = shakeMain $ do
-  want $ [".zuul.yaml"]
-  ".zuul.yaml" %> dhallZuulAction "(./.sf.dhall).zuul"
-  phony "docs" $ do
+buildDocs :: Action ()
+buildDocs =
+  do
+    pdoc3 <- ensurePdoc3
+    args <- getCmdFromTox
+    putInfo $ "args: " <> args
     cmd_ "rm -Rf build/docs"
-    cmd_ "tox -edocs"
+    cmd_ pdoc3 args
     cmd_ "mv build/html/pynotedb build/docs"
+  where
+    -- Returns the pdoc3 args from the tox.ini file
+    getCmdFromTox = do
+      tox <- readFileLines "tox.ini"
+      let toxs = map words tox
+      let cmd = [x | x@("commands" : "=" : "pdoc3" : _) <- toxs]
+      let args = map (dropWhile (/= "pdoc3")) cmd
+      pure (head (map (unwords . drop 1) args))
+    -- Ensures pdoc3 is installed and returns its path
+    ensurePdoc3 = do
+      home <- getEnvWithDefault "/home/user" "HOME"
+      let pdoc3 = home <> "/.local/bin/pdoc3"
+      pdoc3Installed <- doesFileExist pdoc3
+      unless pdoc3Installed (cmd_ "pip3 install --user pdoc3")
+      pure pdoc3
+
+main :: IO ()
+main = shakeMain $ do
+  want [".zuul.yaml"]
+  ".zuul.yaml" %> dhallZuulAction "(./.sf.dhall).zuul"
+  phony "docs" buildDocs
